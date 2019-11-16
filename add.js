54ee1b0b7e9b5c11f4dda42d211a1225b8244982
oyvindsabo <oyvind.sabo@gmail.com>
2019-11-16 14:41:17 +0100

    Promisify readFile

--- add.js
+++ add.js
@@ -1,13 +1,16 @@
 const fs = require('fs');
+const util = require('util');
+
+const readFile = util.promisify(fs.readFile);
 
 const WebAssemblyFunctions = {};
 
-init = async () => {
-  const bytes = fs.readFileSync('./add.wasm');
-  return WebAssembly.instantiate(bytes).then(({ instance }) =>
-    Object.assign(WebAssemblyFunctions, instance.exports)
-  );
-};
+init = async () =>
+  readFile('./add.wasm')
+    .then(WebAssembly.instantiate)
+    .then(({ instance }) =>
+      Object.assign(WebAssemblyFunctions, instance.exports)
+    );
 
 init().then(() => {
   console.log(WebAssemblyFunctions.add(8, 5));
